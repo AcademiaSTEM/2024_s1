ff2889ba0d0a103bba001860870f352a190e0767
lily416 <daconcha@uc.cl>
2024-01-22 00:09:21 -0300

    fix: base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/2024_s1',
   cors: true,
   server: {
     watch: {
